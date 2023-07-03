589fd4c094d188a78f35a726c18b6944e647a02b
Bharath <bharath.alluri389@gmail.com>
2023-07-03 07:49:47 +0530

    server.js changes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+const bodyParser = require('body-parser');
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "public")));
+// Middleware to parse the request body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Define login API endpoint
-app.post("/loginForm", (req, res) => {
+app.post('/api/login', (req, res) => {
   // Retrieve login credentials from the request body
   const { email, password } = req.body;
 
@@ -14,16 +15,11 @@ app.post("/loginForm", (req, res) => {
   // Validate the email and password against a user database or any other data source
   // Return appropriate responses based on the authentication result
 
-  if (email === "example@example.com" && password === "password") {
-    res.status(200).json({ message: "Login successful" });
+  if (email === 'example@example.com' && password === 'password') {
+    res.status(200).json({ message: 'Login successful' });
   } else {
-    res.status(401).json({ message: "Invalid credentials" });
+    res.status(401).json({ message: 'Invalid credentials' });
   }
-});
-
-// Serve index.html for GET requests to the root URL
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Start the server
