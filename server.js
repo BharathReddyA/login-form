3fb397462db88d319ae4912f7d5c3b5cc56bae2e
Bharath <bharath.alluri389@gmail.com>
2023-07-05 17:25:01 +0530

    server.js changed

--- server.js
+++ server.js
@@ -6,19 +6,24 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Define login API endpoint
-app.post('/api/login', (req, res) => {
-  // Retrieve login credentials from the request body
-  const { email, password } = req.body;
+// Define login API endpoint for both GET and POST requests
+app.all('/api/login', (req, res) => {
+  if (req.method === 'GET') {
+    res.send('Please use a POST request to login.');
+  } else if (req.method === 'POST') {
+    // Retrieve login credentials from the request body
+    const { email, password } = req.body;
 
-  // Perform authentication logic here
-  // Validate the email and password against a user database or any other data source
-  // Return appropriate responses based on the authentication result
+    // Perform authentication logic here
+    // ...
 
-  if (email === 'example@example.com' && password === 'password') {
-    res.status(200).json({ message: 'Login successful' });
+    if (email === 'example@example.com' && password === 'password') {
+      res.status(200).json({ message: 'Login successful' });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
   } else {
-    res.status(401).json({ message: 'Invalid credentials' });
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
 });
 
